67150cfb1ac4053a74e9d77c5199ebff4b4d238b
jenil2209 <jenilparmar1999@gmail.com>
2023-08-04 22:06:33 +0530

    Add project files.

---
+++ Models/CalculationHistory.cs
@@ -0,0 +1,12 @@
+﻿// CalculationHistory.cs
+using System;
+
+namespace CalculatorLibrary
+{
+    public class CalculationHistory
+    {
+        public int Id { get; set; }
+        public string Calculation { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ Models/CalculatorLibrary.cs
@@ -0,0 +1,18 @@
+﻿// ArithmeticCalculator.cs
+namespace CalculatorLibrary
+{
+    public class ArithmeticCalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // Add more arithmetic methods here
+    }
+}
