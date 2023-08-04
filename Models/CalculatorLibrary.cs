12a6619466254044eebb3cc4057aea92702bfbad
jenil2209 <jenilparmar1999@gmail.com>
2023-08-05 01:03:53 +0530

    i added ui basics will improve it

--- Models/CalculatorLibrary.cs
+++ Models/CalculatorLibrary.cs
@@ -13,6 +13,21 @@ namespace CalculatorLibrary
             return a - b;
         }
 
-        
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("not valid");
+            }
+
+            return a / b;
+        }
+
+
     }
 }
